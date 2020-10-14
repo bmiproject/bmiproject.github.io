c5cf930f980a97e081e9299f66c32bd1c14c8635
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-10-14 22:33:03 +0800

    add whats great section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
 		<marketplays-applications-section />
 		<sponsors-section />
 		<services-section />
+		<whats-great-section />
 	</div>
 </template>
 
@@ -12,6 +13,7 @@ import HeroSection from '@/components/sections/Hero'
 import MarketplaysApplicationsSection from '@/components/sections/MarketplaysApplications'
 import SponsorsSection from '@/components/sections/Sponsors'
 import ServicesSection from '@/components/sections/Services'
+import WhatsGreatSection from '@/components/sections/WhatsGreat'
 
 export default {
 	name: 'Home',
@@ -19,7 +21,8 @@ export default {
 		HeroSection,
 		MarketplaysApplicationsSection,
 		SponsorsSection,
-		ServicesSection
+		ServicesSection,
+		WhatsGreatSection,
 	}
 }
 </script>
