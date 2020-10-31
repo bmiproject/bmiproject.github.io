61ca435ec58c5430ad3a2c5941d462e15bacebe1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-31 13:57:35 +0000

    Deploy bmiproject/bmiproject.github.io to github.com/bmiproject/bmiproject.github.io.git:gh-pages

---
+++ precache-manifest.7dc0ecc0d1b92bb5095deb4493a38a3a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3397343918053a96f66",
+    "url": "/bmiproject.github.io/css/app.6596d695.css"
+  },
+  {
+    "revision": "413918f0bde8c0efc6ae",
+    "url": "/bmiproject.github.io/css/chunk-1e7c5348.34025acc.css"
+  },
+  {
+    "revision": "845d23e5a5986cdb4c4c",
+    "url": "/bmiproject.github.io/css/chunk-vendors.26f99c50.css"
+  },
+  {
+    "revision": "8ac77eef4156c12f1c1c990280f764f7",
+    "url": "/bmiproject.github.io/img/avatar.8ac77eef.png"
+  },
+  {
+    "revision": "28172a3c96d1791876aa3a67c7b7ee0b",
+    "url": "/bmiproject.github.io/img/cta1.28172a3c.jpg"
+  },
+  {
+    "revision": "81ef0d0e61adbf654f2f6e48da5be730",
+    "url": "/bmiproject.github.io/img/cta2.81ef0d0e.jpg"
+  },
+  {
+    "revision": "6f47b9c9253c2af1a9d5b7a92c22ab61",
+    "url": "/bmiproject.github.io/img/hero.6f47b9c9.jpg"
+  },
+  {
+    "revision": "773de94f0d95a46f18ee9916df3f420f",
+    "url": "/bmiproject.github.io/img/hero.773de94f.jpg"
+  },
+  {
+    "revision": "8d5c0c2026b236d1fd67ab42c459d519",
+    "url": "/bmiproject.github.io/img/howitworks.8d5c0c20.jpg"
+  },
+  {
+    "revision": "55406963dce70e07a7d67e29a6ee7b7b",
+    "url": "/bmiproject.github.io/img/info1.55406963.png"
+  },
+  {
+    "revision": "695161292e3868161cd9e4efd92f9074",
+    "url": "/bmiproject.github.io/img/info2.69516129.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/bmiproject.github.io/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "aac66f56a2f0cfc23e4f53465a547467",
+    "url": "/bmiproject.github.io/img/logo.aac66f56.png"
+  },
+  {
+    "revision": "5cb761035ed1bc3c68a8204260c109cd",
+    "url": "/bmiproject.github.io/img/payment.5cb76103.png"
+  },
+  {
+    "revision": "0f67f28a30862beeed1e237f054f1752",
+    "url": "/bmiproject.github.io/img/service1.0f67f28a.png"
+  },
+  {
+    "revision": "074fcd8420cc951428c9846ba134f97e",
+    "url": "/bmiproject.github.io/img/service2.074fcd84.png"
+  },
+  {
+    "revision": "6dc77cbcfcdfbfb3d518543357d07e1f",
+    "url": "/bmiproject.github.io/img/service3.6dc77cbc.png"
+  },
+  {
+    "revision": "16985bf563755cb8393f6fb378cec580",
+    "url": "/bmiproject.github.io/img/service4.16985bf5.png"
+  },
+  {
+    "revision": "4371e1165fc444c3cda77b43f251ca91",
+    "url": "/bmiproject.github.io/img/testimonial-back.4371e116.jpg"
+  },
+  {
+    "revision": "301b73a889855bdfae0827dc08e18761",
+    "url": "/bmiproject.github.io/index.html"
+  },
+  {
+    "revision": "f3397343918053a96f66",
+    "url": "/bmiproject.github.io/js/app.89bb75da.js"
+  },
+  {
+    "revision": "413918f0bde8c0efc6ae",
+    "url": "/bmiproject.github.io/js/chunk-1e7c5348.54938058.js"
+  },
+  {
+    "revision": "845d23e5a5986cdb4c4c",
+    "url": "/bmiproject.github.io/js/chunk-vendors.feaffa76.js"
+  },
+  {
+    "revision": "142a3bc5ff21a37a74cfb4ff148a0829",
+    "url": "/bmiproject.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bmiproject.github.io/robots.txt"
+  }
+]);
